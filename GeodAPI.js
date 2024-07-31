bddcc75fc6af0a16b358f555aa44a2a48b49c088
Weaver1911 <weaver1911@protonmail.com>
2024-07-31 14:39:23 +0300

    Initial Commit v2

---
+++ GeodAPI.js
@@ -0,0 +1,72 @@
+const { exec } = require('child_process');
+const ping = require('ping');
+const fs = require('fs').promises;
+const puppeteer = require('puppeteer');
+const express = require('express');
+const dotenv = require('dotenv');
+const { parseHtmlTables } = require('./helpers/scraper');  // Import the parseHtmlTables function
+const { loginToDevice, loadCookies } = require('./helpers/login'); // Import login functions
+const { pingIpRange, getIpAddress } = require('./helpers/Discover'); // Import device discovery functions
+
+dotenv.config();
+
+let ipAddress = '';
+let loggedIn = false;
+
+/**
+ * Function to get data from the device and return as JSON.
+ * @param {object} page - The Puppeteer page instance.
+ * @returns {Promise<object>} - The parsed table data.
+ */
+async function getDataFromDevice(page) {
+  await loadCookies(page);
+  await page.goto(`http://${ipAddress}`, { waitUntil: 'networkidle2' });
+
+  const tableData = await parseHtmlTables(page);
+  return tableData;
+}
+
+// Create an Express web server
+const app = express();
+const port = process.env.PORT || 3000; // Use PORT environment variable or default to 3000
+
+// Add a route for the /data endpoint
+app.get('/api', async (req, res) => {
+  try {
+    if (!loggedIn) {
+      console.log('Initializing network...');
+
+      // Attempt to find IP address using MAC address
+      try {
+        ipAddress = await getIpAddress(process.env.MAC_ADDRESS);
+      } catch (err) {
+        // If MAC address lookup fails, run ping method and retry
+        console.log('Executing ping...');
+        const baseIp = process.env.BASE_IP;
+        await pingIpRange(baseIp, 1, 254);
+        ipAddress = await getIpAddress(process.env.MAC_ADDRESS, false);
+        if (!ipAddress) {
+          throw new Error(`Station [${process.env.MAC_ADDRESS}] not found after scanning your network.`);
+        }
+      }
+      
+      loggedIn = await loginToDevice(ipAddress, process.env.PASSWORD);
+    }
+
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+
+    const tableData = await getDataFromDevice(page);
+    
+    await browser.close();
+    res.json(tableData);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Error retrieving data. Offline?');
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Callisto Server is running at http://localhost:${port}/api`);
+});
