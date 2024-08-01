bee9422cb4d3b13fad05cea767672c3d5e3c8714
Weaver <7733981+Weaver1911@users.noreply.github.com>
2024-08-01 13:30:31 +0300

    Startup now includes device discovery and login procedure

--- GeodAPI.js
+++ GeodAPI.js
@@ -30,29 +30,9 @@ async function getDataFromDevice(page) {
 const app = express();
 const port = process.env.PORT || 3000; // Use PORT environment variable or default to 3000
 
-// Add a route for the /data endpoint
+// Add a route for the /api endpoint
 app.get('/api', async (req, res) => {
   try {
-    if (!loggedIn) {
-      console.log('Initializing network...');
-
-      // Attempt to find IP address using MAC address
-      try {
-        ipAddress = await getIpAddress(process.env.MAC_ADDRESS);
-      } catch (err) {
-        // If MAC address lookup fails, run ping method and retry
-        console.log('Executing ping...');
-        const baseIp = process.env.BASE_IP;
-        await pingIpRange(baseIp, 1, 254);
-        ipAddress = await getIpAddress(process.env.MAC_ADDRESS, false);
-        if (!ipAddress) {
-          throw new Error(`Station [${process.env.MAC_ADDRESS}] not found after scanning your network.`);
-        }
-      }
-      
-      loggedIn = await loginToDevice(ipAddress, process.env.PASSWORD);
-    }
-
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
@@ -66,7 +46,32 @@ app.get('/api', async (req, res) => {
   }
 });
 
-// Start the server
-app.listen(port, () => {
+// Start the server and perform the device discovery and login
+app.listen(port, async () => {
   console.log(`Callisto Server is running at http://localhost:${port}/api`);
+
+  try {
+    console.log('Initializing network...');
+
+    // Attempt to find IP address using MAC address
+    try {
+      ipAddress = await getIpAddress(process.env.MAC_ADDRESS);
+    } catch (err) {
+      // If MAC address lookup fails, run ping method and retry
+      console.log('Executing ping...');
+      const baseIp = process.env.BASE_IP;
+      await pingIpRange(baseIp, 1, 254);
+      ipAddress = await getIpAddress(process.env.MAC_ADDRESS, false);
+      if (!ipAddress) {
+        throw new Error(`Station [${process.env.MAC_ADDRESS}] not found after scanning your network.`);
+      }
+    }
+    
+    loggedIn = await loginToDevice(ipAddress, process.env.PASSWORD);
+    if (loggedIn) {
+      console.log('Ready to serve requests!');
+    }
+  } catch (error) {
+    console.error('Error during startup:', error);
+  }
 });
